9a08422b9d2cb8394a8f4f53037a79ed5f368808
kennethteg <tegradok@gmail.com>
2021-12-07 13:17:44 +0800

    init exer 9 for cmsc 12

---
+++ Main Exercises/TEGRADOK_exer10/script.js
@@ -0,0 +1,12 @@
+/*
+Exercise 10: Machine Organization; JavaScript - introduction, syntax, variables, expressions, control structures, and functions 
+
+CMSC 12 | Course
+
+Submitted to:
+Professor Aragones
+
+Submitted by:
+TEGRADO, Kenneth Renz A.
+
+*/
