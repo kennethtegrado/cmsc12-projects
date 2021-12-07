eaea90e4f4e7808ace963481b5567dee7f891efc
kennethteg <tegradok@gmail.com>
2021-12-07 13:21:19 +0800

    init strict mode

--- Main Exercises/TEGRADOK_exer10/script.js
+++ Main Exercises/TEGRADOK_exer10/script.js
@@ -8,5 +8,18 @@ Professor Aragones
 
 Submitted by:
 TEGRADO, Kenneth Renz A.
-
 */
+
+/*
+Tasks:
+Write a JavaScript function that takes an array of 8 positive integers as input parameter, and returns an array of integers containing only the numbers that are either: 
+    > divisible by 3; OR 
+    > even but NOT divisible by 4 
+*/
+
+// Start of the main program
+
+// Use Strict Mode
+'use strict';
+
+// Init Global Variables
