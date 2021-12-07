ed55c08e9ca1d4d059d86843c912890c2bc1d767
kennethteg <tegradok@gmail.com>
2021-12-07 17:46:25 +0800

    init files for the exercise

---
+++ Main Exercises/TEGRADOK_exer11/script.js
@@ -0,0 +1,11 @@
+/*
+Exercise 11 - Programming Languages, JavaScript - capturing HTML inputs and JavaScript events
+
+CMSC 12 | Course
+
+Submitted to:
+Professor Aragones
+
+Submitted by:
+TEGRADO, Kenneth Renz A.
+*/
